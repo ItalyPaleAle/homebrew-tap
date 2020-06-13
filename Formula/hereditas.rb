43e851a5b297da23120672a872e57a12fbc1461e
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-06-13 16:03:16 -0700

    Linting Hereditas

--- Formula/hereditas.rb
+++ Formula/hereditas.rb
@@ -1,7 +1,7 @@
 require "language/node"
 
 class Hereditas < Formula
-  desc "Static site generator for a fully trustless digital legacy box "
+  desc "Static site generator for a fully trustless digital legacy box"
   homepage "https://hereditas.app"
   url "https://registry.npmjs.org/hereditas/-/hereditas-0.2.1.tgz"
   sha256 "e1e1cb375dc55b1e04220b2c2082bf9fc5b257fc8ea11e7db7edfd01afe7f71a"
@@ -14,6 +14,6 @@ class Hereditas < Formula
   end
 
   test do
-    assert_match /hereditas\/0\.2\.1/, shell_output("#{bin}/hereditas --version")
+    assert_match /%r{hereditas/0\.2\.1}/, shell_output("#{bin}/hereditas --version")
   end
 end
