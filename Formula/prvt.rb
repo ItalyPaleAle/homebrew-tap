7e3140d2a739fb60d7df41116e88112e9472d3b0
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-07-02 22:28:28 -0700

    packr shipped as formula is broken

--- Formula/prvt.rb
+++ Formula/prvt.rb
@@ -5,7 +5,6 @@ class Prvt < Formula
   sha256 "1c282c6c48a596f64adf0f815eb6185fdecd05e9df4b0a9fc6ab677496099c40"
 
   depends_on "go" => ["1.14", :build]
-  depends_on "packr" => ["2.7", :build]
 
   def install
     ENV["GOPATH"] = buildpath
@@ -14,16 +13,21 @@ class Prvt < Formula
     ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
     (buildpath/"src/github.com/ItalyPaleAle/prvt").install buildpath.children
     cd "src/github.com/ItalyPaleAle/prvt" do
+      # Fetch the UI
       system "curl", "-L",
         "https://github.com/ItalyPaleAle/prvt/releases/download/" \
         "v0.4.2/prvt-v0.4.2-ui.tar.gz",
         "-o dist.tar.gz"
-      system "tar", "-xzf dist.tar.gz"
+      system "tar", "-xvzf dist.tar.gz"
       rm_rf "ui/dist"
       mv "dist", "ui/dist"
-      system "ls", "ui/"
-      system "ls", "ui/dist/"
-      system "packr"
+      # Fetch packr (the formula on homebrew is broken)
+      system "curl", "-LO",
+        "https://github.com/gobuffalo/packr/releases/download/"\
+        "v2.7.1/packr_2.7.1_darwin_amd64.tar.gz"
+      system "tar", "-xvzf packr_2.7.1_darwin_amd64.tar.gz"
+      system "packr_2.7.1_darwin_amd64/packr2"
+      # Build the app
       system "go", "build", "-ldflags",
         "-X github.com/ItalyPaleAle/prvt/buildinfo.AppVersion=v0.4.2 " \
         "-X github.com/ItalyPaleAle/prvt/buildinfo.BuildID=0.4.2 " \
