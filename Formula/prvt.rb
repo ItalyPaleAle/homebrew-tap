ab64962a3a38cbd4bc64a4a1949ce837b3b89216
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-07-02 21:57:44 -0700

    Using pre-compiled UI

--- Formula/prvt.rb
+++ Formula/prvt.rb
@@ -1,7 +1,7 @@
 class Prvt < Formula
   desc "Store end-to-end encrypted files and view them in your browser"
   homepage "https://github.com/ItalyPaleAle/prvt"
-  url "https://github.com/ItalyPaleAle/prvt/archive/v0.4.1.tar.gz"
+  url "https://github.com/ItalyPaleAle/prvt/archive/v0.4.2.tar.gz"
   sha256 "3e9ecefff72bba5f7f7834f442bdd9b6ee953306bbbf0f4bd1ecccaf7af9b09b"
 
   depends_on "go" => ["1.14", :build]
@@ -13,16 +13,20 @@ class Prvt < Formula
     ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
     (buildpath/"src/github.com/ItalyPaleAle/prvt").install buildpath.children
     cd "src/github.com/ItalyPaleAle/prvt" do
+      system "curl", "-Ls", "https://github.com/ItalyPaleAle/prvt/releases/download/v0.4.2/prvt-v0.4.2-ui.tar.gz", "-o dist.tar.gz"
+      system "tar", "-xzf", "dist.tar.gz"
+      system "rm", "-rf", "ui/dist"
+      system "mv", "dist", "ui/"
       system "go", "build", "-ldflags",
-        "-X github.com/ItalyPaleAle/prvt/buildinfo.AppVersion=v0.4.1 " \
-        "-X github.com/ItalyPaleAle/prvt/buildinfo.BuildID=0.4.1 " \
+        "-X github.com/ItalyPaleAle/prvt/buildinfo.AppVersion=v0.4.2 " \
+        "-X github.com/ItalyPaleAle/prvt/buildinfo.BuildID=0.4.2 " \
         "-X github.com/ItalyPaleAle/prvt/buildinfo.BuildTime=brew",
-        "-X github.com/ItalyPaleAle/prvt/buildinfo.CommitHash=v0.4.1",
+        "-X github.com/ItalyPaleAle/prvt/buildinfo.CommitHash=v0.4.2",
         "-o", bin/"prvt", "."
     end
   end
 
   test do
-    assert_match /v0\.4\.1/, shell_output("#{bin}/prvt version")
+    assert_match /v0\.4\.2/, shell_output("#{bin}/prvt version")
   end
 end
