39b6b3e16355e6e4c44a8e950439206216cee56e
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-09-10 00:53:27 +0000

    Trying to make the linter happy

--- Casks/prvt.rb
+++ Casks/prvt.rb
@@ -1,23 +1,23 @@
-cask 'prvt' do
-  version '0.5.0'
-  sha256 'a7eff5105fe8e8414cbfb2198109f8e4e8cec0e056fe8208cea0dc750e5632ae'
-  url "https://github.com/ItalyPaleAle/prvt/releases/download/v#{version}/prvt-v#{version}-macos.tar.gz"
+cask "prvt" do
+  version "0.5.0"
+  sha256 "a7eff5105fe8e8414cbfb2198109f8e4e8cec0e056fe8208cea0dc750e5632ae"
 
-  name 'prvt'
+  url "https://github.com/ItalyPaleAle/prvt/releases/download/v#{version}/prvt-v#{version}-macos.tar.gz"
+  appcast "https://github.com/ItalyPaleAle/prvt/releases.atom"
+  name "prvt"
   desc "Store end-to-end encrypted files and view them in your browser"
   homepage "https://github.com/ItalyPaleAle/prvt"
 
-  appcast "https://github.com/ItalyPaleAle/prvt/releases.atom"
+  depends_on macos: ">= :sierra"
 
-  depends_on macos: '>= :sierra'
   binary "prvt-v#{version}-macos/prvt"
 
   # Remove the quarantine because the binary isn't signed
   preflight do
-    system_command 'xattr',
+    system_command "xattr",
                    args: [
-                           '-dr', 'com.apple.quarantine',
-                           "#{staged_path}/prvt-v#{version}-macos/prvt"
-                         ]
+                            "-dr", "com.apple.quarantine",
+                            "#{staged_path}/prvt-v#{version}-macos/prvt"
+                          ]
   end
 end
